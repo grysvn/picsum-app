99cef619491bb8fd13e5e814e01cace476532ce3
Matthew Gray <grysvn@Matthews-MBP.lan>
2022-01-12 14:37:06 -0500

    Creates prototype collection view cell and corresponding file.

---
+++ Picsum App/Picsum App/UI/PicsumCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PicsumCollectionViewCell.swift
+//  Picsum App
+//
+//  Created by Matthew Gray on 1/12/22.
+//
+
+import Foundation
+import UIKit
+
+class PicsumCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
--- Picsum App/Picsum App/ViewController.swift
+++ Picsum App/Picsum App/ViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let reuseIdentifier = "pictureCell"
+    var items: [PicsumModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        PicsumInteractor.shared.retrieveImages(limit: 100) { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let responseModel):
+                self.items = responseModel
+                self.reload()
+            case .failure(_):
+                self.items = [] // expand on error state here
+                self.reload()
+            }
+        }
     }
 
-
+    func reload() {
+        
+    }
 }
 
