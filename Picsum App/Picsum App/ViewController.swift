37a4d0c5a16d4e85de343b69fc76e5e235d4449f
Matthew Gray <grysvn@Matthews-MBP.lan>
2022-01-12 13:43:58 -0500

    Adds initial xcode project.

---
+++ Picsum App/Picsum App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picsum App
+//
+//  Created by Matthew Gray on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
